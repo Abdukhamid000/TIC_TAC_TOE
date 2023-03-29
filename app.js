b15000c34a6a4ca2c37d290365cce832aa27cef8
Abdukhamid <Abdukhamid000@gmail.com>
2023-03-29 08:06:20 +0500

    changes

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const newGameMenu = document.querySelector("[data-newGameMenu]");
 const gameBoard = document.querySelector("[data-gameBoard]");
 const cellElements = document.querySelectorAll("[data-cell]");
 const squares = document.querySelector(".squares");
+const turnSvg = document.querySelector("#turnSvg");
+const winnerSvg = document.querySelector("#winnerSvg");
+const bgEffect = document.querySelector("[data-effect]");
+const winningPlayer = document.querySelector("[data-winning-player]");
+const quitBtn = document.querySelector("[data-quit]");
+const nextRoundBtn = document.querySelector("[data-next-round]");
 
 // --------------------------------   X   CIRCLE CLASSES   ------------------------------------------ //
 
@@ -38,6 +44,7 @@ playerModeBtn.addEventListener("click", () => {
 
 // ---------------------------   GAME FUNCTIONALITY   ----------------------------------------- //
 let xTurn = true;
+const stack = [];
 
 const placeMark = (cell, currentClass) => {
   cell.classList.add(currentClass);
@@ -51,9 +58,11 @@ const setHoverClass = () => {
   if (xTurn) {
     squares.classList.remove(PLAYER_O_CLASS);
     squares.classList.add(PLAYER_X_CLASS);
+    turnSvg.src = "./icons/x (2).svg";
   } else {
     squares.classList.remove(PLAYER_X_CLASS);
     squares.classList.add(PLAYER_O_CLASS);
+    turnSvg.src = "./icons/o.svg";
   }
 };
 setHoverClass();
@@ -66,17 +75,47 @@ const checkForWin = (currentClass) => {
   });
 };
 
+const drawWinner = (currentClass, cell) => {
+  // cellElements.every((el) => console.log(el.classList.contains(currentClass)));
+  console.log(
+    cellElements.forEach((el) => {
+      if (el && el.classList.contains(currentClass)) {
+        if (currentClass === PLAYER_O_CLASS) {
+          el.classList.add("winningCellBgO");
+        } else {
+          el.classList.add("winningCellBgX");
+        }
+      }
+    })
+  );
+
+  bgEffect.classList.replace("hide", "show");
+  if (currentClass === PLAYER_X_CLASS) {
+    winnerSvg.src = "./icons/winnerX.svg";
+    winningPlayer.textContent = "PLAYER 1 WINS!";
+    console.log(cell);
+  } else {
+    winnerSvg.src = "./icons/winnerO.svg";
+    winningPlayer.textContent = "PLAYER 2 WINS!";
+  }
+};
+
 const handleClick = (e) => {
   const cell = e.target;
   const currentClass = xTurn ? PLAYER_X_CLASS : PLAYER_O_CLASS;
 
   placeMark(cell, currentClass);
-  if (checkForWin(currentClass)) console.log("winner" + currentClass);
-
+  if (checkForWin(currentClass)) {
+    drawWinner(currentClass, cell);
+  }
   changeTurns();
   setHoverClass();
 };
 
 cellElements.forEach((cell) => {
   cell.addEventListener("click", handleClick, { once: true });
+});
+
+quitBtn.addEventListener("click", (e) => {
+  window.location.reload();
 });
